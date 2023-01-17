216eb2cab9d3fcfc9e2d1a2091e368e42a000dd6
mdtthanh <maithanh0131@gmail.com>
2023-01-18 00:41:29 +0700

    add img slide

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import requests from "./../Requests";
 import { useState, useEffect } from "react";
 import axios from "axios";
+
 const Main = () => {
   const [movies, setMovies] = useState([]);
 
@@ -11,18 +12,40 @@ const Main = () => {
       setMovies(response.data.results);
     });
   }, []);
+
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
   console.log(movies);
   return (
-    <div className="w-full h-[550] text-white">
-      <div className="w-full h-[550px] bg-gradient-to-r from-black">
+    <div className="w-full h-[600px] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[600px] bg-gradient-to-r from-black"></div>
         <img
           className="w-full h-full object-cover"
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
-          alt="{movie?.title}"
+          alt={movie?.title}
         />
-        <div>
-            <button>Play</button>
-            <button>Watch</button>
+        <div className="absolute w-full top-[20%] p-4 md:p-8 ">
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <div className="my-4">
+            <button className="border bg-gray-300 border-gray-300 text-black py-2 px-5">
+              Play
+            </button>
+            <button className="border text-white border-gray-300 py-2 px-5 ml-4">
+              Watch Later
+            </button>
+          </div>
+          <p className="text-gray-400 text-sm">
+            Released: {movie?.release_date}
+          </p>
+          <p className="w-full md:max-w-[70%] lg:max=w=[50%] xl:max-w-[35%] text-gray-200">
+            {truncateString(movie?.overview, 150)}
+          </p>
         </div>
       </div>
     </div>
