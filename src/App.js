ee9e5350033b767afa6e5b27c0fe177a82eb6667
mdtthanh <maithanh0131@gmail.com>
2023-01-21 19:44:45 +0700

    feat login & logout

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import { AuthContextProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Account from './pages/Account';
+import ProtectRoute from "./components/ProtectRoute";
 function App() {
   return (
     <>
       <AuthContextProvider>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
-          </Routes>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/account' element={
+            <ProtectRoute>
+              <Account/>
+            </ProtectRoute>
+          } />
+        </Routes>
       </AuthContextProvider>
 
 
